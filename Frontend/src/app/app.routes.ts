abd09bf2ba1e3bdbfcf81c0cea4d7ac9d86d132b
agbarbie <barbarawangui2002@gmail.com>
2025-08-18 13:43:02 +0300

    employer-training

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,20 +4,22 @@ import { AuthComponent } from './components/auth/auth.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { DashboardComponent } from './components/jobseeker/dashboard/dashboard.component';
 import { JobExplorerComponent } from './components/jobseeker/job-explorer/job-explorer.component';
-import { TrainingComponent } from './components/jobseeker/training/training.component';
+import { TrainingComponent as JobseekerTrainingComponent } from './components/jobseeker/training/training.component';
 import { EmployerDashboardComponent } from './components/employer/employer-dashboard/employer-dashboard.component';
-import { PostJobComponent } from './components/employer/post-job/post-job.component';
+import { TrainingComponent as EmployerTrainingComponent } from './components/employer/training/training.component';
+import { PostJobComponent } from './components/employer/post-job/post-job.component'; 
 
 export const routes: Routes = [
- { path: '', component: LandingComponent },
- { path: 'auth', component: AuthComponent },
- { path: 'reset-password', component: ResetPasswordComponent },
- {path: 'jobseeker', redirectTo: 'jobseeker/dashboard'},
- {path: 'jobseeker/dashboard', component: DashboardComponent },
- {path: 'jobseeker/job-explorer', component: JobExplorerComponent },
- {path: 'job-explorer', redirectTo: 'jobseeker/job-explorer'},
- {path: 'jobseeker/training', component: TrainingComponent },
- {path: 'employer', redirectTo: 'employer/employer-dashboard'},
- {path: 'employer/employer-dashboard', component: EmployerDashboardComponent },
- { path: 'post-job', component: PostJobComponent },
+  { path: '', component: LandingComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'jobseeker', redirectTo: 'jobseeker/dashboard' },
+  { path: 'jobseeker/dashboard', component: DashboardComponent },
+  { path: 'jobseeker/job-explorer', component: JobExplorerComponent },
+  { path: 'job-explorer', redirectTo: 'jobseeker/job-explorer' },
+  { path: 'jobseeker/training', component: JobseekerTrainingComponent },
+  { path: 'employer', redirectTo: 'employer/employer-dashboard' },
+  { path: 'employer/employer-dashboard', component: EmployerDashboardComponent },
+  { path: 'employer/training', component: EmployerTrainingComponent },
+  { path: 'employer/post-job', component: PostJobComponent }, 
 ];
