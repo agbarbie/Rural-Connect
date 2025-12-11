1b22efd01b3a67e8416400ab99db36f35c07fa8d
Barbara <barbarawangui2002@gmail.com>
2025-12-11 16:12:02 +0300

    Candidates tha applied for the job

---
+++ Frontend/src/app/components/employer/candidate-profile/candidate-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-profile',
+  imports: [],
+  templateUrl: './candidate-profile.component.html',
+  styleUrl: './candidate-profile.component.css'
+})
+export class CandidateProfileComponent {
+
+}
