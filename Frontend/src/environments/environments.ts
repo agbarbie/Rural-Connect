9d228ce7e9b6a1122dd9e462c762e1ada98f8a60
Barbara <barbarawangui2002@gmail.com>
2026-02-14 08:23:28 +0300

    Updated links for prod

--- Frontend/src/environments/environments.ts
+++ Frontend/src/environments/environments.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'https://rural-connect-3.onrender.com/api'
 };
