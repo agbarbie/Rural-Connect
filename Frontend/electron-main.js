51507153cef83c4ab4d784d997b7bb3445a4e7c2
Barbara <barbarawangui2002@gmail.com>
2026-04-29 12:50:28 +0300

    feat: upgrade to version 1.0.0 and add Electron support
    - Updated package version to 1.0.0
    - Added Electron main file (electron-main.js) to create a desktop application
    - Included scripts for starting and building the Electron app
    - Configured electron-builder for packaging the application for Windows
    - Updated dependencies to include Electron and electron-builder

---
+++ Frontend/electron-main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false
+    },
+    title: 'RuralConnect'
+  });
+
+  win.loadFile(path.join(__dirname, 'dist/ruralconnect/browser/index.html'));
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
